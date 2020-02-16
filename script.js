636f6d0a6615f5570f283b9923a5b327fff293f1
Jakub Sadowski <jaakub.sadowski@gmail.com>
2020-02-16 16:18:50 +0100

    Implement checkLength, checkEmail and checkPasswordsMatch function

--- script.js
+++ script.js
@@ -16,19 +16,40 @@ const showSuccess = (input) => {
    formControl.className = 'form-control success';
 }
 
-const isValidEmail = (email) => {
+const checkEmail = (input) => {
    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-   return re.test(String(email).toLowerCase());
+
+   if (re.test(input.value.trim())) {
+      showSuccess(input);
+   } else {
+      showError(input, 'Email is not valid!')
+   }
 }
 
 const checkRequired = (inputArr) => {
-   inputArr.forEach((input) => {
+   inputArr.forEach(input => {
       if (input.value.trim() === '') {
          showError(input, `${getFieldName(input)} is required!`);
       } else {
          showSuccess(input);
       }
    });
+}
+
+const checkLength = (input, min, max) => {
+   if (input.value.length < min) {
+      showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+   } else if (input.value.length > max) {
+      showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+   } else {
+      showSuccess(input);
+   }
+}
+
+const checkPasswordsMatch = (input1, input2) => {
+   if (input1.value !== input2.value) {
+      showError(input2, 'Passwords do not match!');
+   }
 }
 
 const getFieldName = (input) => {
@@ -39,4 +60,8 @@ form.addEventListener('submit', (e) => {
    e.preventDefault();
 
    checkRequired([username, email, password, password2]);
+   checkLength(username, 3, 10);
+   checkLength(password, 6, 15);
+   checkEmail(email);
+   checkPasswordsMatch(password, password2)
 })
